f6d6f848d0ed70b35daabc477676216745b5f05a
noggythenog1 <michael.green.edu@gmail.com>
2021-07-27 14:22:00 +0100

    UnitTestLesson refactored

--- UNITTESTLESSON/CODETOTEST/Program.cs
+++ UNITTESTLESSON/CODETOTEST/Program.cs
@@ -6,7 +6,27 @@ namespace CODETOTEST
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int timeOfDay = 21;
+            var myGreeting = Greeting(timeOfDay);
+            Console.WriteLine(myGreeting);
+        }
+
+        private static string Greeting(int timeOfDay)
+        {
+            string greeting;
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                greeting ="Good morning!";
+            }
+            else if (timeOfDay >= 12 && timeOfDay <= 18)
+            {
+                greeting = "Good afternoon!";
+            }
+            else
+            {
+                greeting = "Good evening!";
+            }
+            return greeting;
         }
     }
 }
