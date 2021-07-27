f2ae9d2e5929e8721b0853f298082c484ab4c6de
noggythenog1 <michael.green.edu@gmail.com>
2021-07-27 13:52:25 +0100

    UnitTestLesson solution added

---
+++ UNITTESTLESSON/CODETOTEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CODETOTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
